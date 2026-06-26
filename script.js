35dce841a4de131315f1d1479b89bcf42dd73de3
VlctorLuo <24176vl@hvhs.school.nz>
2026-06-26 15:12:53 +1200

    did some of the form stuff

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello world!")
+const OUTPUT = document.getElementById("outputField")
+
+function getFormInput(){
+            const FIGURE_NAME = document.getElementById("figureName")
+        if (FIGURE_NAME.checkValidity()=== true) {
+        var figureName = FIGURE_NAME.value;
+        OUTPUT.innerHTML = "<p>You added "+ figureName+ " to your cart.</p>"
+    } else if (FIGURE_NAME.checkValidity()=== false) {
+        OUTPUT.innerHTML = "<p>Please enter a valid name.</p>"
+    }
+}
+
+/*
+function getFormInput2(){
+    const NAME_FIELD = document.getElementById("nameField");
+    let userName = NAME_FIELD.value;
+    OUTPUT.innerHTML = "<h1>Your name is "+ userName + "</h1>"
+}
+*/
